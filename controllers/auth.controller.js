51ad68858ecd4c75682b4c87e813747c159b5dc8
LakshyaHub77 <Lakshya@techarchsoftwares.in>
2026-05-08 00:56:56 +0530

    changes

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -44,12 +44,12 @@ export const login = async (req, res) => {
 
     let token = await genToken(user._id);
 
-    res.cookie("token", token, {
-      httpOnly: true,
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-      secure: process.env.NODE_ENVIRONMENT === "production",
-      sameSite: "lax",
-    });
+res.cookie("token", token, {
+  httpOnly: true,
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+  secure: true,
+  sameSite: "none",
+});
 
     return res.status(200).json({ message: "User logged in successfully", user });
   } catch (err) {
@@ -59,10 +59,10 @@ export const login = async (req, res) => {
 
 
 export const logout = async (req, res) => {
-  res.clearCookie('token', {
-    httpOnly: true,
-    secure: process.env.NODE_ENVIRONMENT === 'production',
-    sameSite: 'strict',
-  });
+ res.clearCookie("token", {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+});
   return res.status(200).json({ message: "Logged out successfully" });
 };
